7c726e9dc1204b4debb99c259572533878da216f
alecs297 <alexandru.nicolae.moldovan@eisti.fr>
2021-03-30 13:39:24 +0200

    renamed stuff

--- README.md
+++ README.md
@@ -1,6 +1,44 @@
 # eisti-celcat
 
+## Description
+
 Extracts the current month's planning from celcat and converts to ics.
 
-`index.js` uses puppeteer, slow but it's the first version so I'm keeping it for the memes
-`clean.js` uses fetch, way faster but not easily readable
+`puppeteer-version.js` uses puppeteer, slow but it's the first version so I'm keeping it for the memes
+
+`index.js` uses direct requests
+
+Will gather data from the previous month to the following year.
+
+## Requirements
+
+> - nodejs
+> - puppeteer (for the puppeteer version)
+
+## Dependencies
+
+Listed in package.json
+
+## Instalation
+
+> - Clone the repo
+> - Rename settings.json.example to settings.json and change "login" and "password".
+> - Install the dependencies via `npm install`
+
+for the puppeteer version, install puppeteer via `npm install puppeteer`
+
+## Usage
+
+`node index.js`
+
+## TODO
+
+```none
+remove the puppeteer part
+multi user support
+command line arguments
+```
+
+## Other versions
+
+a go version, which is faster, can be found [here](https://github.com/Obito1903/CY-celcat-to-ics)
---
+++ puppeteer-version.js
@@ -0,0 +1,57 @@
+const puppeteer = require('puppeteer');
+const settings = require('./settings.json');
+const moment = require('moment');
+const ics = require('ics');
+const fs = require('fs');
+const he = require('html-entities');
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function pressEnter(page) {
+    await page.keyboard.press(String.fromCharCode(13));
+}
+
+async function type(page, id, text) {
+    await page.focus(id);
+    await page.keyboard.type(text);
+}
+
+function parseDesc(text) {
+    text = he.decode(text.replace(/\r\n/g, "").replace(/<br \/>/g, "\n")).split("\n");
+    return [
+        [text[0]], text[1], text.slice(2).join('\n')
+    ]
+}
+
+(async() => {
+    const browser = await puppeteer.launch({
+        headless: true,
+    });
+
+    const page = await browser.newPage();
+
+    await page.goto(settings.login_url);
+    await type(page, settings.login_id, settings.login);
+    await type(page, settings.passw_id, settings.password);
+    await pressEnter(page);
+
+    page.on('response', async(res) => {
+        if (res.url().startsWith(settings.response_url)) {
+            let data = (await res.json()).map(x => { return { "start": moment(x.start).format('YYYY-M-D-H-m').split("-"), "end": moment(x.end).format('YYYY-M-D-H-m').split("-"), "description": parseDesc(x.description)[2], "title": parseDesc(x.description)[1], "categories": parseDesc(x.description)[0], "startOutputType": "local" } });
+            ics.createEvents(data, async(e, v) => {
+                if (!e) {
+                    try {
+                        await fs.writeFileSync(settings.ics_path, v);
+                    } catch (error) {
+                        console.log(error);
+                    }
+                } else {
+                    console.log(e);
+                }
+                await browser.close();
+            })
+        }
+    })
+})();
